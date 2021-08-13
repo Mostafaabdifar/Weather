8be77d8b41dc94470018007d3b7cbf2fe6e82f21
Mostafa <mostafaabdi1998@gmail.com>
2021-08-13 14:38:24 +0430

    add icon section

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 const form = document.getElementById('select-city')
 const selectDropdown = document.querySelector('.city');
 const sectionApi = document.querySelector('.api-section');
-
+const msg = document.querySelector('.msg');
+const KELVIN = 273;
 const key = 'c0b2fde154e22b98a8313be787127295';
-const KELVIN = 273
-
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   let city = selectDropdown.value;
   getSearchCity(city);
-
 });
-
 
 function getSearchCity(city) {
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
-
   fetch(api)
     .then(response => response.json())
     .then(data => {
@@ -25,65 +21,38 @@ function getSearchCity(city) {
         name,
         weather
       } = data;
-      sectionApi.innerHTML = `<div class = "name-location">${name}</div> <div>${weather[0].description}</div> <div class="temp">${Math.floor(main.temp - KELVIN)}<sup>°C</sup></div>`
-    })
-    .then(function (data) {
-      const temp = data.main[0];
-      const weatherDes = data.weather[0].description;
-      const nameCity = data.name;
-    })
+      const desWeather = weather[0].description;
 
+      function checkImg() {
+        let pic;
+        switch (desWeather) {
+          case "clear sky":
+            pic = 'sunny.svg';
+            break;
+          case "broken clouds":
+            pic = 'clouds.svg';
+            break;
+          case "few clouds":
+            pic = 'cloud.svg';
+            break;
+          case "scattered clouds":
+            pic = 'cloud.svg';
+            break;
+          case "rain":
+            pic = 'rain.svg';
+            break;
+        }
+        return pic;
+      }
+      const icon = checkImg();
+
+      /* we can use the following code for the icon section:
+      const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+      <img class="shape-weather" src="${icon}" alt="alt=${weather[0]["main"]}">*/
+
+      sectionApi.innerHTML = `<div class = "name-location">${name}</div> <div class="description">${weather[0].description}</div> <div class="temp">${Math.floor(main.temp - KELVIN)}<sup>°C</sup></div> <img class="shape-weather" src="img/${icon}" alt="${weather[0]["main"]}">`
+    })
+    .catch(() => {
+      msg.textContent = "Server Error!";
+    });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const getCity = async(city) => {
-//   const url = `'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}'`
-// }
-
-// function instruction() {
-//   fetch(url)
-//     .then(response => response.json())
-//   console.log
-//     .then(data => {
-//       const {
-//         main,
-//         name,
-//         weather
-//       } = data;
-
-//     city.innerHTML = `<h3 class = "name-city">${name}</h3>`;
-//   weatherCity.innerHTML = `<p class="status-weather">${weather[0]["description"]}</p>`;
-//   degree.innerHTML = `${(main.temp)- 273}`;
-
-//   switch (weatherCity){
-//     case clouds:
-
-//   }
-// });
-
-// }
